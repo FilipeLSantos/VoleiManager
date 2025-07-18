c5ba11d16010415f46abd358816478b65009a1b5
Filipe Santos <filipelopesdossantos85@gmail.com>
2025-07-18 12:10:09 -0300

    Conflito de merge no persistence corrigido

--- src/main/java/io/github/guisso/javasepersistencewithhibernateorm/beta/repository/DataSourceFactory.java
+++ src/main/java/io/github/guisso/javasepersistencewithhibernateorm/beta/repository/DataSourceFactory.java
@@ -33,7 +33,7 @@ import jakarta.persistence.Persistence;
 public class DataSourceFactory {
 
     private static EntityManagerFactory emf;
-    private static final String PU_NAME = "gestaoEscolarBetaPU";
+    private static final String PU_NAME = "voleimanagerPU";
 
     // EntityManagerFactory can have a persistent lifecycle
     private static EntityManagerFactory getEMFactory() {
