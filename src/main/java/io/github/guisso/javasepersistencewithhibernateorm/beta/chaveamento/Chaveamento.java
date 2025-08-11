7d801431cb3964cca4efabfc30ed12a237983aac
Samuel Paranhos <sanparanhos@gmail.com>
2025-08-11 20:53:26 -0300

    Criação da Entidade Chaveamento

---
+++ src/main/java/io/github/guisso/javasepersistencewithhibernateorm/beta/chaveamento/Chaveamento.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright (C) 2025 SamuelParanhos
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package io.github.guisso.javasepersistencewithhibernateorm.beta.chaveamento;
+
+import io.github.guisso.javasepersistencewithhibernateorm.beta.repository.ProjectEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.io.Serializable;
+
+/**
+ *
+ * @author SamuelParanhos
+ */
+@Entity
+public class Chaveamento extends ProjectEntity implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+   
+    @Column(nullable = false)
+    private String Evento;
+    
+    @Column(nullable = false)
+    private boolean lixo;
+    
+    @Column(nullable = false)
+    private String campeao;
+
+    public String getEvento() {
+        return Evento;
+    }
+
+    public void setEvento(String Evento) {
+        this.Evento = Evento;
+    }
+
+    public boolean isLixo() {
+        return lixo;
+    }
+
+    public void setLixo(boolean lixo) {
+        this.lixo = lixo;
+    }
+
+    public String getCampeao() {
+        return campeao;
+    }
+
+    public void setCampeao(String campeao) {
+        this.campeao = campeao;
+    }
+    
+    
+
+    @Override
+    public String toString() {
+        return "Chaveamento{" + "Evento=" + Evento + ", lixo=" + lixo + ", campeao=" + campeao + '}';
+    }
+}
